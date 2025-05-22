9ec4017bbbe7aaafcead3a9bfa93a4c12ca67666
justliya <justliya@icloud.com>
2025-05-21 20:03:28 -0500

    created company research prompt

--- jobsearch_agents/jobsearch_agents/agent.py
+++ jobsearch_agents/jobsearch_agents/agent.py
@@ -7,6 +7,7 @@ from .shared_libraries import constants
 
 from .sub_agents.listing.agent import listing_search_agent
 
+from .sub_agents.research.agent import company_research_agent
 
 from . import prompt
 MODEL = "gemini-2.0-flash-exp"
@@ -18,6 +19,7 @@ root_agent = Agent(
     instruction=prompt.ROOT_PROMPT,
     sub_agents=[
         listing_search_agent,
+        company_research_agent,
       
     ],
 )
