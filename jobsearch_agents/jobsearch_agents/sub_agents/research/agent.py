432cdeff85c9d344a9a29d82358f54975eabb594
LaKaleigh Harris <toronto324@gmail.com>
2025-06-03 19:31:57 -0400

    feat: integrate Google Maps API with MCPToolset in company research agent

--- jobsearch_agents/jobsearch_agents/sub_agents/research/agent.py
+++ jobsearch_agents/jobsearch_agents/sub_agents/research/agent.py
@@ -2,9 +2,10 @@
 from google.adk.agents.llm_agent import LlmAgent
 from ...shared_libraries import constants
 from . import prompt
-from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset
+from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset, StdioServerParameters
 from google.adk.tools.mcp_tool.mcp_session_manager import StreamableHTTPServerParams
 
+google_maps_api_key = os.environ.get("GOOGLE_MAPS_API_KEY")
 
 company_research_agent = LlmAgent(
     model=constants.MODEL,
@@ -25,6 +26,25 @@ company_research_agent = LlmAgent(
                 'get_company_salaries_glassdoor',
                 'get_company_interviews',
                 'get_company_salary'
+            ]
+        ),
+        MCPToolset(
+            connection_params=StdioServerParameters(
+                command='npx',
+                args=[
+                    "-y",
+                    "@modelcontextprotocol/server-google-maps",
+                ],
+                env={
+                    "GOOGLE_MAPS_API_KEY": google_maps_api_key
+                },
+            ),
+            tool_filter=[
+                'maps_geocode',
+                'maps_search_places',
+                'maps_place_details',
+                'maps_distance_matrix',
+                'maps_directions'
             ]
         )
     ]
